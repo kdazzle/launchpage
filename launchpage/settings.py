5b83ab003f2a787b2065bc701fb3f7326d18ab65
Kyle Valade <kylevalade@gmail.com>
2012-06-25 20:22:26 -0700

    Changed the settings file so that the database can sync

--- launchpage/settings.py
+++ launchpage/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             # Or path to database file if using sqlite3.
-        'NAME': (os.path.join(PROJECT_DIR, '..', 'db', 'tempDb.db'),),
+        'NAME': os.path.join(PROJECT_DIR, '..', 'db', 'tempDb.db'),
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
